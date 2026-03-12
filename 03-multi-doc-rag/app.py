4f3c8f2df2bd360e69322ca96f3d64ab31f7ccab
giri-www <girirahul944@gmail.com>
2026-03-12 18:27:10 +0530

    FEAT : MAIN APP DILE ADDED

--- 03-multi-doc-rag/app.py
+++ 03-multi-doc-rag/app.py
@@ -1,3 +1,23 @@
-""" 
+from pipeline.rag_pipeline import RAGPipeline
 
-"""
+
+def main():
+
+    rag = RAGPipeline()
+
+    while True:
+
+        query = input("\nAsk question: ")
+
+        answer, sources = rag.ask(query)
+
+        print("\nAnswer:\n", answer)
+
+        print("\nSources:")
+
+        for s in sources:
+            print(f"{s['source']} page {s['page']}")
+
+
+if __name__ == "__main__":
+    main()
