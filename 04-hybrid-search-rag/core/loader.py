c4c41148bf93fc2015c2115190ef91789905af92
giri-www <girirahul944@gmail.com>
2026-03-13 17:00:09 +0530

    Updated : fixed loaded

--- 04-hybrid-search-rag/core/loader.py
+++ 04-hybrid-search-rag/core/loader.py
@@ -1,7 +1,7 @@
 import os
 import logging
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 logger = logging.getLogger(__name__)
 
