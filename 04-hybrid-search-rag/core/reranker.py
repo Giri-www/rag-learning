3b2abc28361ebdeda84d950bf8976a298a7b9b93
giri-www <girirahul944@gmail.com>
2026-03-13 16:32:37 +0530

    FEAT : Reranked code added for filtering ranking filter

---
+++ 04-hybrid-search-rag/core/reranker.py
@@ -0,0 +1,24 @@
+from sentence_transformers import CrossEncoder
+
+
+class Reranker:
+
+    def __init__(self):
+
+        self.model = CrossEncoder(
+            "BAAI/bge-reranker-base"
+        )
+
+    def rerank(self, query, docs):
+
+        pairs = [[query, d.page_content] for d in docs]
+
+        scores = self.model.predict(pairs)
+
+        ranked = sorted(
+            zip(docs, scores),
+            key=lambda x: x[1],
+            reverse=True
+        )
+
+        return [d for d, _ in ranked[:5]]
