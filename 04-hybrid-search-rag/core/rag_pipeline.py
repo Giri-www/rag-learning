a50b0a0c04509e5b34e4de951f93b530bfe0384d
giri-www <girirahul944@gmail.com>
2026-03-14 14:21:06 +0530

    FEAT : (rag_pipeline.py ) Logger and langsmith monitoring addded

--- 04-hybrid-search-rag/core/rag_pipeline.py
+++ 04-hybrid-search-rag/core/rag_pipeline.py
@@ -1,17 +1,30 @@
+from tracemalloc import start
+
 import ollama
+from langsmith import traceable
+from monitoring.logger import PipelineLogger
+import time
+
+logger = PipelineLogger()
 
 class RagPipeline:
 
     def __init__(self, retriever, reranker):
         self.retriever = retriever
         self.reranker = reranker
-
+    @traceable
     def run(self, query: str) -> str:
+        
 
-        docs = self.retriever.retrieve(query)
+        logger.log_query(query)
        
-        reranked_docs = self.reranker.rerank(query, docs)
+        docs = self.retriever.retrieve(query)
+        logger.log_retrieval(vector_docs=docs, keyword_docs=[])
 
+        reranked_docs = self.reranker.rerank(query, docs)
+        logger.log_rerank(reranked_docs)
+        start = time.time()
+        
         contxt = ""
 
         for doc in reranked_docs:
@@ -23,5 +36,9 @@ class RagPipeline:
             model="llama3",
             messages=[{"role": "user", "content": prompt}] 
         )
+        
+        latency = time.time() - start
+
+        logger.log_llm(latency)
 
         return response["message"]["content"]
