55c8c33f3ebd0f00bb9d7862dffde5a2a09e54f5
giri-www <girirahul944@gmail.com>
2026-03-14 14:20:22 +0530

    updated logger : Straem lite

--- 04-hybrid-search-rag/app/streamlit_app.py
+++ 04-hybrid-search-rag/app/streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import sys
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from core.loader import DocumentLoader
